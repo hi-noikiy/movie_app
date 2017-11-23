ee576b7a5152a5444365aafe01a903036f6a700d
silentsvv <silentsvv@163.com>
2017-11-23 23:20:46 +0800

    init: 初始化

---
+++ src/pages/Index/Index.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1 class="test">sssssssssssssssssssss</h1>
+</template>
+
+<style lang="scss">
+@import '../../scss/mixin.scss';
+
+.test {
+  @include fixImage('../../assets','games.png');
+}
+</style>
+
